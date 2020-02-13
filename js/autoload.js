e2649fb64dfac2bf7288d4b20600e245ecf4c675
HCLonely <h1606051253@gmail.com>
2020-02-13 19:12:22 +0800

    Site updated: 2020-02-13 19:11:52

--- js/autoload.js
+++ js/autoload.js
@@ -145,7 +145,7 @@ try {
         initModel(waifuTipsJson);
     }
     let cn=window.location.hostname==='blog.hclonely.cn';
-    let siteDiv = $(`<div class="menus_item" title="${cn ? "国外" : "中国大陆"}用户访问更快"><a class="site-page" href="${cn ? "//blog.hclonely.com" : "//blog.hclonely.cn"}"><i class="fa-fw fab fa-chrome"></i><span> ${cn ? "源站" : "镜像站"}</span></a></div>`);
+    let siteDiv = $(`<div class="menus_item" title="${cn ? "海外" : "中国大陆"}用户访问更快"><a class="site-page" href="${cn ? "//blog.hclonely.com" : "//blog.hclonely.cn"}"><i class="fa-fw fab fa-chrome"></i><span> ${cn ? "源站" : "镜像站"}</span></a></div>`);
     siteDiv.hover(function () { $(this).find("i").addClass("turn-around") }, function () { $(this).find("i").removeClass("turn-around") })
     $("head").append(`<style>.turn-around{-webkit-animation: avatar_turn_around 0.1s linear infinite;-moz-animation: avatar_turn_around 0.1s linear infinite;-o-animation: avatar_turn_around 0.1s linear infinite;-ms-animation: avatar_turn_around 0.1s linear infinite;animation: avatar_turn_around 0.1s linear infinite;}</style>`);
     $(".menus_item:first").after(siteDiv);
