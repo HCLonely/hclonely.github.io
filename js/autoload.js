05623d636bd00528ff04a21523c041d75eba34de
HCLonely <h1606051253@gmail.com>
2020-03-02 18:41:06 +0800

    Site updated: 2020-03-02 18:41:05

--- js/autoload.js
+++ js/autoload.js
@@ -135,7 +135,7 @@ try {
         $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
         /* 可直接修改部分参数 */
         live2d_settings['modelAPI'] = '//live2d.hclonely.com/';
-        live2d_settings['staticAPI'] = '//hclonely-model-cn.oss-cn-shanghai.aliyuncs.com';
+        live2d_settings['staticAPI'] = '//model.hclonely.com';
         live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
         live2d_settings['modelId'] = 105;                  // 默认模型 ID
         live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
