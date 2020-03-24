f7fdcdbe505eda53809f0b81e4be4a400e87658c
HCLonely <h1606051253@gmail.com>
2020-03-24 22:32:02 +0800

    Site updated: 2020-03-24 22:32:01

--- js/main.js
+++ js/main.js
@@ -271,16 +271,16 @@ $(function () {
    * 代碼收縮
    */
   const $highlightTools = $('.highlight-tools')
-  if (isHighlightShrink === true) {
+  if (isHighlightShrink === 'true') {
     $highlightTools.append('<i class="fa fa-angle-down code-expand code-closed" aria-hidden="true"></i>')
-  } else if (isHighlightShrink === false) {
+  } else if (isHighlightShrink === 'false') {
     $highlightTools.append('<i class="fa fa-angle-down code-expand" aria-hidden="true"></i>')
   }
 
   $(document).on('click', '.highlight-tools >.code-expand', function () {
     var $table = $(this).parent().next()
     if ($(this).hasClass('code-closed')) {
-      $table.css('display', '')
+      $table.css('display', 'block')
       $(this).removeClass('code-closed')
     } else {
       $table.css('display', 'none')
