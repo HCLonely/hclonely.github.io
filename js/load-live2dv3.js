1720fb5634ac563692635a4e2d4567a8902167b5
HCLonely <h1606051253@gmail.com>
2020-04-02 21:33:13 +0800

    Site updated: 2020-04-02 21:33:12

--- js/load-live2dv3.js
+++ js/load-live2dv3.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   if (navigator.userAgent.match(/Mobile/i)) return
   const models = {
-    'l2dmodelv3_1@1.0.0': [
+    'l2dmodelv3_1@1.0.1': [
       'aidang_2',
       'aierdeliqi_4',
       'aierdeliqi_5',
@@ -11,14 +11,14 @@ window.onload = () => {
       'biaoqiang',
       'biaoqiang_3'
     ],
-    'l2dmodelv3_2@1.0.0': [
+    'l2dmodelv3_2@1.0.1': [
       'bisimai_2', 'chuixue_3',
       'dafeng_2', 'dafeng_4',
       'deyizhi_3', 'dujiaoshou_4',
       'dunkeerke_2', 'genaisennao_2',
       'heitaizi_2'
     ],
-    'l2dmodelv3_3@1.0.0': [
+    'l2dmodelv3_3@1.0.1': [
       'huangjiafangzhou_3',
       'huonululu_3',
       'huonululu_5',
@@ -28,7 +28,7 @@ window.onload = () => {
       'lafei_4',
       'lingbo'
     ],
-    'l2dmodelv3_4@1.0.0': [
+    'l2dmodelv3_4@1.0.1': [
       'longfeng_2',
       'mingshi',
       'nengdai_2',
@@ -38,7 +38,7 @@ window.onload = () => {
       'qibolin_2',
       'shengluyisi_2'
     ],
-    'l2dmodelv3_5@1.0.0': [
+    'l2dmodelv3_5@1.0.1': [
       'shengluyisi_3',
       'sipeibojue_5',
       'taiyuan_2',
@@ -49,21 +49,18 @@ window.onload = () => {
       'xianghe_2',
       'xixuegui_4'
     ],
-    'l2dmodelv3_6@1.0.0': [
+    'l2dmodelv3_6@1.0.3': [
       'xuefeng',
       'xuefeng_3',
       'xukufu_2',
       'yichui_2',
       'z23',
       'z46_2',
-      'zhala_2',
       'kubo_2',
       'junhe_5'
     ],
-    'l2dmodelv3_7@0.0.1': [
-      'shengluyisi_2',
-      'weiyan_2',
-      'wuqi_2',
+    'l2dmodelv3_7@0.0.2': [
+      'zhala_2',
       'z46_3'
     ]
   }
@@ -76,7 +73,7 @@ window.onload = () => {
       }
     }
   }
-  const l2dmodel = getModel(Math.ceil(new Date().getTime() / (1000 * 60 * 60 * 24)) % 48)
+  const l2dmodel = getModel(Math.ceil(new Date().getTime() / (1000 * 60 * 60 * 24)) % 52)
   new l2dViewer({
     el: document.getElementById('L2dCanvas'),
     basePath: 'https://cdn.jsdelivr.net/npm/' + l2dmodel[0],
