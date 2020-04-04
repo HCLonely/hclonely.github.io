c0baa57f08652b7be91d89401fe48d9978375712
HCLonely <h1606051253@gmail.com>
2020-04-05 00:03:24 +0800

    Site updated: 2020-04-05 00:03:22

--- js/load_map.js
+++ js/load_map.js
@@ -1,1 +1,12 @@
-0<$("#visitors-map").length?navigator.userAgent.match(/Mobile/i)?$("#visitors-map").append('<script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330&cl=ffffff&w=a"><\/script>'):$("#visitors-map").append('<script type="text/javascript" id="clstr_globe" src="//cdn.clustrmaps.com/globe.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330"><\/script>'):($("head").append("<style>.clustrmaps-map-control{display:none !important;}</style>"),$("body").append('<script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330&cl=ffffff&w=a"><\/script>'));
+window.onload=()=>{
+	if($("#visitors-map").length>0){
+		if (navigator.userAgent.match(/Mobile/i)) {
+			$("#visitors-map").append('<script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330&cl=ffffff&w=a"></script>');
+		}else{
+			$("#visitors-map").append('<script type="text/javascript" id="clstr_globe" src="//cdn.clustrmaps.com/globe.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330"></script>');
+		}
+	}else{
+		$("head").append("<style>.clustrmaps-map-control{display:none !important;}</style>");
+		$("body").append('<script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?d=7RJAye3Doa8wj5huc-j4LftrwmQQMkdycswG6qp6330&cl=ffffff&w=a"></script>');
+	}
+}
